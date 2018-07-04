a7fc54d27c5d8e8279e31f1f02075526a0cf26d2
11223ren <1132974092@qq.com>
2018-07-04 14:17:55 +0800

    恢复金额

--- application/User/Controller/InfoController.class.php
+++ application/User/Controller/InfoController.class.php
@@ -239,7 +239,7 @@ class InfoController extends MemberbaseController {
     public function alipay(){
         $payment=I('post.payment','');
         $money=I('post.money','');
-        $money=0.01;
+       
         if($payment!='alipay'){
             $this->error('错误地址');
         }
@@ -337,7 +337,7 @@ class InfoController extends MemberbaseController {
     public function wxpay(){
         $payment=I('post.payment','');
         $money=I('post.money','');
-        $money=0.01;
+        
         if($payment!='wxpay'){
             $this->error('错误地址');
         }
