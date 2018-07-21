81f8355cc99aa43516e821b87076fa7fc4b72840
smilesjuan <38411688+smilesjuan@users.noreply.github.com>
2018-07-21 14:52:26 +0800

    测试

--- themes/simplebootx/Public/js/load.js
+++ themes/simplebootx/Public/js/load.js
@@ -248,39 +248,19 @@ $(document).ready(function(){
 
 
 	$("input[type='text']").keyup(function () {
-		noEmoji($(this));
+		var param = $(this).val();
+		console.log(param);
+		console.log("pp");
+		var regRule = /[^\u0020-\u007E\u00A0-\u00BE\u2E80-\uA4CF\uF900-\uFAFF\uFE30-\uFE4F\uFF00-\uFFEF\u0080-\u009F\u2000-\u201f\u2026\u2022\u20ac\r\n]/g;
+
+		if(param.match(param)){
+			param = param.replace("regRule","");
+			alert("111");
+		}
+
 	});
 
-	var noEmoji = function (dom) {
-		var html = "",
-			oldHtml = "";
-		if ('INPUT' == dom.tagName) {
-			html = $(dom).val();
-		}
-		oldHtml = html;
-		var reg = /[^\u0020-\u007E\u00A0-\u00BE\u2E80-\uA4CF\uF900-\uFAFF\uFE30-\uFE4F\uFF00-\uFFEF\u0080-\u009F\u2000-\u201f\u2026\u2022\u20ac\r\n]/g;
-		if (html.match(reg)) {
-			html = html.replace(reg, '');
-		}
-
-		if (html != oldHtml) {
-			if ('INPUT' == dom.tagName) {
-				$(dom).val(html);
-			}
-		}
-		/**
-		 * 光标移到最后
-		 */
-		function endFocus(dom) {
-			var sel = window.getSelection();
-			var range = document.createRange();
-			range.selectNodeContents(dom);
-			range.collapse(false);
-			sel.removeAllRanges();
-			sel.addRange(range);
-		}
-	};
-
+	
 
 
 
