0cf0acc9e5811a43dc64ac71a623e8d738da1fbd
smilesjuan <38411688+smilesjuan@users.noreply.github.com>
2018-07-30 09:46:24 +0800

    移动端图片大小提示

--- themes/simplebootx/Public/js/personal.js
+++ themes/simplebootx/Public/js/personal.js
@@ -6,7 +6,8 @@ function preview(file,o)
      // 验证文件大小
     var fileSize = file.files[0].size / 1024;
     if (fileSize > 4 * 1024) {
-        alert("图片大小不能超过4MB");
+        // alert("图片大小不能超过4MB");
+        $(".msg4").text("图片大小不能超过4MB");
         $(file).val('');
         return false;
     } 
